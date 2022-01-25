ba7b42c7be83731ad730b8edf71e822f77dc7f30
abxlfazl khxrshidi <abolfazlkhorshidi99@gmail.com>
2022-01-25 20:27:56 +0330

    add utils

---
+++ src/lib/utils/index.js
@@ -0,0 +1,107 @@
+class Utils {
+	extraInputsFilter = (input) => {
+		const getTheNumberSign = (pattern = "") =>
+			new RegExp(
+				`((?<ope>[-+×÷])${pattern}|^${pattern})(?<num>\\d+(\\.\\d*)?(e[+-]\\d+)?)=?±$`,
+				"g"
+			);
+
+		return input
+			.join("")
+			.replace(/\.+/g, ".")
+			.replace(/^\.|((?<=[-+×÷])|-?\d+(\.?\d*)?(e[+-]\d+)?=)\./g, "0.")
+			.replace(/(?<=\d{16})\.$/g, "")
+			.replace(/\d+(\.\d*)?$/g, (result) =>
+				result.includes(".") ? result.slice(0, 17) : result.slice(0, 16)
+			)
+			.replace(/\d+(\.?\d*)?(e[+-]\d+)?(?==?%)/g, (num) => {
+				return this.roundingTheResult(num / 100);
+			})
+			.replace(/%/, "")
+			.replace(
+				/^0±|(?<=\d+(\.?\d*)?(e[+-]\d+)?[-+×÷])0±|-0\.?0*(?=[-+×÷])|^0+|(?<=[-+×÷])0+|∞=/g,
+				"0"
+			)
+			.replace(/(^0|(?<=[-+×÷%])0)(?<num>[1-9])/g, "$<num>")
+			.replace(getTheNumberSign("-"), "$<ope>$<num>")
+			.replace(getTheNumberSign(), "$<ope>-$<num>")
+			.replace(
+				/^-?\d+(\.?\d*)?(e[+-]\d+)?=(?<num>\d+(\.?\d*)?(e[+-]\d+)?)/g,
+				"$<num>"
+			)
+			.replace(
+				/(?<num>-?\d+(\.?\d*)?(e[+-]\d+)?)(?<ope>[-+×÷%])=/g,
+				"$<num>$<ope>$<num>="
+			)
+			.replace(/(?<ope>[-+×÷=%](?!\d+(\.?\d*)?(e[+-]\d+)?))+/g, "$<ope>")
+			.replace(
+				/∞(?=.)|(?<=^-?\d+(\.?\d*)?(e[+-]\d+)?[-+×÷%]?)=$|(?<=[-+×÷%]|(-?\d+(\.?\d*)?(e[+-]\d+)?[-+×÷%]\d+(\.?\d*)?(e[+-]\d+)?))±|(?<=\d+\.\d+)\.|(\.0*|(?<=\d+\.(0*[1-9]+)*)0+)(?=[-+=×÷%])/g,
+				""
+			);
+	};
+	matchTheEquation = (input, pattern) => {
+		const regex = new RegExp(
+			`^-?\\d+(\\.?\\d*)?(e[+-]\\d+)?[-+×÷]-?\\d+(\\.?\\d*)?(e[+-]\\d+)?${pattern}$`,
+			"g"
+		);
+		return input?.match(regex);
+	};
+	spacingBetweenOperations = (input) => {
+		return input?.replace(/(?<=-?(\d+(\.?\d*)?(e[+-]\d+)?))[-+=×÷]/g, " $& ");
+	};
+	showFirstEntry = (input) => {
+		return this.spacingBetweenOperations(
+			input
+				.join("")
+				.match(/^-?\d+(\.?\d*)?(e[+-]\d+)?[-+×÷%]/g)
+				?.join("")
+		);
+	};
+	showLastEntry = (input) => {
+		return input
+			.join("")
+			.match(/((?<=[-+×÷])|^)-?\d+(\.?\d*)?(e[+-]\d+)?[-+=×÷]?$|∞/g)
+			?.join("")
+			.replace(/(?<=\d+(\.?\d*)?)[-+=×÷%]/g, "")
+			.replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+			.replace(/(?<=\..+),+/g, "");
+	};
+	roundingTheResult = (input) => {
+		if (!isFinite(input)) {
+			return String(input).replace(/Infinity/g, "∞");
+		}
+
+		return String(input)
+			.replace(/-?\d+\.\d+/g, (value) =>
+				Number(Math.round(value + "e6") + "e-6")
+			)
+			.replace(/\d+\.?\d*e[+-]\d+|^-?\d{17,}$/g, (value) =>
+				Number.parseFloat(value).toExponential(6)
+			)
+			.replace(/(?<=[1-9]+|\d+\.)0+(?=e)|e\+0/g, "");
+	};
+	repeatEqualOperations = (input, replace) => {
+		return input.replace(
+			/-?\d+(\.?\d*)?(e[+-]\d+)?(?=[-+×÷]-?\d+(\.?\d*)?(e[+-]\d+)?=)/g,
+			replace
+		);
+	};
+	isFinite = (input) => {
+		return /∞/.test(input.join(""));
+	};
+	theSliceKeyShouldWork = (input) => {
+		return !/^(0|-?\d+(\.?\d*)?[-+×÷=]0?)$/.test(input.join(""));
+	};
+	sliceValue = (input) => {
+		return input
+			.join("")
+			.replace(/\d+(\.?\d*)?$/g, (str) => str.slice(0, -1))
+			.replace(/^-?\d+(\.?\d*)?[-+×÷]$/g, "$&0")
+			.replace(/^('')?$|^-$|((?<=-?\d+(\.?\d*)?[-+×÷])-|(?<=[-+×÷])-0)$/g, 0)
+			.split("");
+	};
+}
+
+const utils = new Utils();
+
+export default utils;

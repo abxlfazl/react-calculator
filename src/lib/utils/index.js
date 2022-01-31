2baca15469b93c07c7b7b27355a480145f22f061
abxlfazl khxrshidi <abolfazlkhorshidi99@gmail.com>
2022-01-31 17:42:02 +0330

    fix issues utils

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -4,20 +4,20 @@ class Utils {
 	extraInputsFilter = (input) => {
 		const getTheNumberSign = (pattern = "") =>
 			new RegExp(
-				`((?<ope>[-+×÷])${pattern}|^${pattern})(?<num>\\d+(\\.\\d*)?(e[+-]\\d+)?)=?±$`,
+				`((?<ope>[-+×÷])${pattern}|^${pattern})(?<num>\\d+(\\.\\d*)?(e[+-]\\d+)?)(=|%)?±$`,
 				"g"
 			);
 
 		return input
 			.join("")
 			.replace(/\.+/g, ".")
-			.replace(/((?<=[-+×÷])|-?\d+(\.?\d*)?(e[+-]\d+)?=)\./g, "0.")
+			.replace(/((?<=[-+×÷])|-?\d+(\.?\d*)?(e[+-]\d+)?(=|%))\./g, "0.")
 			.replace(/(?<=\d{16})\.$/g, "")
 			.replace(/\d+(\.\d*)?$/g, (result) =>
 				result.includes(".") ? result.slice(0, 17) : result.slice(0, 16)
 			)
 			.replace(
-				/-?0\.?%|^0±|(?<=\d+(\.?\d*)?(e[+-]\d+)?[-+×÷])0±|-0\.?0*(?=[-+×÷])|^0+|(?<=[-+×÷])0+|∞=/g,
+				/^-?0\.?%|(?<=[-+×÷])-?0\.?%$|^0±|(?<=\d+(\.?\d*)?(e[+-]\d+)?[-+×÷])0±|-0\.?0*(?=[-+×÷])|^0+|(?<=[-+×÷])0+|∞=/g,
 				"0"
 			)
 			.replace(/(^0|(?<=[-+×÷%])0)(?<num>[1-9])/g, "$<num>")
@@ -64,7 +64,7 @@ class Utils {
 		return this.spacingBetweenOperations(
 			input
 				.join("")
-				.match(/^-?\d+(\.?\d*)?(e[+-]\d+)?[-+×÷]/g)
+				.match(/^-?\d+(\.?\d*)?(e[+-]\d+)?[-+×÷%]/g)
 				?.join("")
 		);
 	};
@@ -83,13 +83,13 @@ class Utils {
 		}
 
 		return String(input)
-			.replace(/-?\d+\.\d+/g, (value) =>
-				Number(Math.round(value + "e6") + "e-6")
-			)
-			.replace(/\d+\.?\d*e[+-]\d+|^-?\d{17,}$/g, (value) =>
+			.replace(/\d+\.?\d*e[+-]\d+|^-?(\d{17,}|\d{16,}\.\d+)$/g, (value) =>
 				Number.parseFloat(value).toExponential(6)
 			)
-			.replace(/(?<=[1-9]+|\d+\.)0+(?=e)|e\+0/g, "");
+			.replace(/(?<=[1-9]+|\d+\.)0+(?=e)|e\+0/g, "")
+			.replace(/-?\d+\.\d+/g, (value) =>
+				Number(Math.round(value + "e6") + "e-6")
+			);
 	};
 	repeatEqualOperations = (input, replace) => {
 		return input.replace(
@@ -101,7 +101,7 @@ class Utils {
 		return /∞/.test(input.join(""));
 	};
 	theSliceKeyShouldWork = (input) => {
-		return !/^(0|-?\d+(\.?\d*)?([-+×÷=]0?|e[+-]\d+([-+×÷=%]0?)?))$/.test(
+		return !/(^0|-?\d+(\.?\d*)?([-+×÷=%]0?|e[+-]\d+([-+×÷=%]0?)?))$/.test(
 			input.join("")
 		);
 	};
